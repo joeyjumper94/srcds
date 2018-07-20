7a676ac13381ea6ac100224c7081bba3df6e4a42
joeyjumper94 <joeyjumper94@gmail.com>
2018-07-19 19:03:24 -0700

    fixed some stuff

--- tf2/install_tf2.bat
+++ tf2/install_tf2.bat
@@ -3,5 +3,5 @@ title install_tf2.bat
 %~dp0..\steamcmd\steamcmd.exe +login anonymous +force_install_dir %~dp0 +app_update 232250 validate +quit
 xcopy /y %~dp0\bin2 %~dp0\bin
 xcopy /y/s %~dp0\platform2\* %~dp0\platform\*
-pause
+timeout 10
 exit
