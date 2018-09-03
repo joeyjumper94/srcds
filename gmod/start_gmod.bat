c01196e099a8364158c45494ce7d4ee5abb1dc6e
joeyjumper94 <joeyjumper94@gmail.com>
2018-09-02 19:45:49 -0700

    updated the start.bat for gmod

--- gmod/start_gmod.bat
+++ gmod/start_gmod.bat
@@ -1,16 +1,18 @@
 @echo off
-cls
 echo Protecting srcds from crashes...
 echo If you want to close srcds and this script, close the srcds window and type Y depending on your language followed by Enter.
 title start_gmod.bat
-start stop_gmod.bat
 :srcds
+start stop_gmod.bat
 echo (%time%) srcds started.
 ::don't touch anything above here
-start /wait srcds.exe -insecure -console -game garrysmod +host_workshop_collection COLLECTION_ID +map gm_flatgrass +gamemode sandbox +hostname "equestrian wastelanders GMod"
+if EXIST .\srcds_x64.exe (
+	start /wait srcds_x64.exe -insecure -console +map gm_construct +gamemode "sandbox" +hostname "Equestrian Wastelanders GMOD" +host_workshop_collection ""
+) else (
+	start /wait srcds.exe -insecure -console +map gm_construct +gamemode "sandbox" +hostname "Equestrian Wastelanders GMOD" +host_workshop_collection ""
+)
 ::don't touch anything below here
 echo (%time%) WARNING: srcds closed or crashed, restarting.
 taskkill /fi "windowtitle eq stop_gmod.bat"
 TIMEOUT 1
-start stop_gmod.bat
 goto srcds
