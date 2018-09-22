981e8824b5459a8f5f5d93bb71dca52982015f0f
joeyjumper94 <joeyjumper94@gmail.com>
2018-09-22 01:41:59 -0700

    enclosing the folder in quotation marks should fix the problem with folders that have spaces in their name

--- hl2/install_ep2.bat
+++ hl2/install_ep2.bat
@@ -5,6 +5,6 @@ echo type your name and password into the following prompts,
 echo if you use steam guard mobile authentication, you'll have to enter that as well
 set /p name="type your steam account username>"
 set /p pass="type your steam account password>"
-%~dp0..\steamcmd\steamcmd.exe +login %name% %pass% +force_install_dir %~dp0 +app_update 420 validate +quit
+"%~dp0"..\steamcmd\steamcmd.exe +login %name% %pass% +force_install_dir "%~dp0" +app_update 420 validate +quit
 TIMEOUT 10
 exit
--- tf2/install_tf2.bat
+++ tf2/install_tf2.bat
@@ -1,7 +1,7 @@
 @echo off
 title install_tf2.bat
-%~dp0..\steamcmd\steamcmd.exe +login anonymous +force_install_dir %~dp0 +app_update 232250 -beta default validate +quit
-xcopy /y %~dp0\bin2 %~dp0\bin
-xcopy /y/s %~dp0\platform2\* %~dp0\platform\*
+"%~dp0"..\steamcmd\steamcmd.exe +login anonymous +force_install_dir "%~dp0" +app_update 232250 -beta default validate +quit
+xcopy /y "%~dp0"\bin2 "%~dp0"\bin
+xcopy /y/s "%~dp0"\platform2\* "%~dp0"\platform\*
 timeout 10
 exit
